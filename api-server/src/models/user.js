28a580774fc5b7db55c2061bb1e82bf4ebb2750e
Mykyta <nikita2001395@gmail.com>
2024-06-22 22:41:15 +0300

    add: handleMongooseError helper

--- api-server/src/models/user.js
+++ api-server/src/models/user.js
@@ -1,6 +1,8 @@
 const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 
+const { handleMongooseError } = require("../helpers");
+
 const emailReg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
 
 const userSchema = new Schema(
@@ -28,6 +30,8 @@ const userSchema = new Schema(
   },
   { versionKey: false, timestamps: true }
 );
+
+userSchema.post("save", handleMongooseError);
 
 const User = model("user", userSchema);
 
