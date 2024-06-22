e4347ab0119b06533d24cb44711c642bb79acf0a
Mykyta <nikita2001395@gmail.com>
2024-06-22 22:35:32 +0300

    add: register controller

--- api-server/src/routes/api/users.js
+++ api-server/src/routes/api/users.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 
+const { validateBody } = require("../../middlewares");
+const { validationSchemes } = require("../../models/user");
+const ctrl = require("../../controllers/users");
+
 const router = Router();
 
-router.post("/register", (_, res) => {
-  res.json({ message: "In work" });
-});
+router.post("/register", validateBody(validationSchemes.user), ctrl.register);
 
 router.post("/login", (_, res) => {
   res.json({ message: "In work" });
