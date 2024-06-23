35a78e9be3c66845bb0e9bf7f5a15060c7194bea
Mykyta <nikita2001395@gmail.com>
2024-06-23 15:15:38 +0300

    add: authenticate middleware

--- api-server/src/routes/api/users.js
+++ api-server/src/routes/api/users.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 
-const { validateBody } = require("../../middlewares");
+const { validateBody, authenticate } = require("../../middlewares");
 const { validationSchemes } = require("../../models/user");
 const ctrl = require("../../controllers/users");
 
@@ -10,7 +10,7 @@ router.post("/register", validateBody(validationSchemes.user), ctrl.register);
 
 router.post("/login", validateBody(validationSchemes.login), ctrl.login);
 
-router.post("/logout", (_, res) => {
+router.post("/logout", authenticate, (_, res) => {
   res.json({ message: "In work" });
 });
 
