96c63bcfb442b4bf9eab75dbae17a01e4f1bdc2e
Mykyta <nikita2001395@gmail.com>
2024-06-24 15:54:59 +0300

    add: init-db

---
+++ api-server/src/init-db.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const fs = require("node:fs/promises");
+const path = require("node:path");
+
+const { Background } = require("./models/background");
+const { Icon } = require("./models/icon");
+
+const envPath = path.join(__dirname, "..", ".env");
+
+require("dotenv").config({ path: envPath });
+
+const { MONGO_DB_HOST } = process.env;
+
+const initDbDataPath = path.join(__dirname, "..", "fixtures", "init-db");
+
+(async () => {
+  try {
+    await mongoose.connect(MONGO_DB_HOST);
+    console.log("Database connection successful");
+
+    const backgroundData = await fs.readFile(
+      path.join(initDbDataPath, "backgrounds.json"),
+      { encoding: "utf-8" }
+    );
+    await Background.insertMany(JSON.parse(backgroundData));
+
+    const iconData = await fs.readFile(
+      path.join(initDbDataPath, "icons.json"),
+      { encoding: "utf-8" }
+    );
+    await Icon.insertMany(JSON.parse(iconData));
+    process.exit(1);
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+})();
