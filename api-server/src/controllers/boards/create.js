916424c7a2f373c6db15575b1f4340d31427dc5b
Mykyta <nikita2001395@gmail.com>
2024-06-25 17:20:57 +0300

    change: board getAll and create controllers

--- api-server/src/controllers/boards/create.js
+++ api-server/src/controllers/boards/create.js
@@ -3,7 +3,10 @@ const { Board } = require("../../models/board");
 const create = async (req, res) => {
   const { _id: owner } = req.user;
   const newBoard = await Board.create({ ...req.body, owner });
-  res.json(newBoard);
+  const board = await Board.findById(newBoard._id)
+    .populate({ path: "background icon", select: "-createdAt -updatedAt" })
+    .select("-createdAt -updatedAt -owner");
+  res.json(board);
 };
 
 module.exports = create;
--- api-server/src/controllers/boards/getAll.js
+++ api-server/src/controllers/boards/getAll.js
@@ -2,7 +2,9 @@ const { Board } = require("../../models/board");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const boards = await Board.find({ owner });
+  const boards = await Board.find({ owner })
+    .populate({ path: "background icon", select: "-createdAt -updatedAt" })
+    .select("-createdAt -updatedAt -owner");
   res.json(boards);
 };
 
