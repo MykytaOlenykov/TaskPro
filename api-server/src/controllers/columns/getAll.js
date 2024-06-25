0e0c7445a48809e5f56579646a0a00996309fa74
Mykyta <nikita2001395@gmail.com>
2024-06-25 20:31:55 +0300

    fix: column getAll controller

--- api-server/src/controllers/columns/getAll.js
+++ api-server/src/controllers/columns/getAll.js
@@ -1,9 +1,16 @@
+const { Board } = require("../../models/board");
 const { Column } = require("../../models/column");
 const { HttpError } = require("../../helpers");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
   const { board_id } = req.query;
+
+  const board = await Board.findOne({ owner, _id: board_id });
+
+  if (!board) {
+    throw HttpError(404);
+  }
 
   if (!board_id) {
     throw HttpError(400, "board_id query is required");
