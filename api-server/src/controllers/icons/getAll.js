ab57332aaa0179f6d438292cf75093d67b6b53f6
Mykyta <nikita2001395@gmail.com>
2024-06-25 09:01:47 +0300

    change: icons getAll controller

--- api-server/src/controllers/icons/getAll.js
+++ api-server/src/controllers/icons/getAll.js
@@ -1,25 +1,8 @@
-const fs = require("node:fs/promises");
-const path = require("node:path");
-
 const { Icon } = require("../../models/icon");
-
-const rootPath = path.join(__dirname, "..", "..");
 
 const getAll = async (_, res) => {
   const icons = await Icon.find();
-
-  const svgs = [];
-
-  for (const { _id, url } of icons) {
-    const svgContent = await fs.readFile(path.join(rootPath, url), "utf8");
-    svgs.push({
-      _id,
-      url,
-      svg: svgContent,
-    });
-  }
-
-  res.json(svgs);
+  res.json(icons);
 };
 
 module.exports = getAll;
