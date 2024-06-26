25560be7c6c4f607d438c49e80a4231f06206196
Mykyta <nikita2001395@gmail.com>
2024-06-26 21:40:18 +0300

    fix: columns deleteById controller

--- api-server/src/controllers/columns/deleteById.js
+++ api-server/src/controllers/columns/deleteById.js
@@ -1,14 +1,18 @@
 const { Column } = require("../../models/column");
+const { Task } = require("../../models/task");
 const { HttpError } = require("../../helpers");
 
 const deleteById = async (req, res) => {
   const { id } = req.params;
   const { _id: owner_id } = req.user;
-  const result = await Column.findOneAndDelete({ _id: id, owner_id });
+  const result = await Column.exists({ _id: id, owner_id });
 
   if (!result) {
     throw HttpError(404);
   }
+
+  await Task.deleteMany({ column_id: id, owner_id });
+  await Column.deleteOne({ _id: id });
 
   res.status(204).send();
 };
