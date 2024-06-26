117652fff9a0c2100cb3815f392a4a50227e4fc9
Mykyta <nikita2001395@gmail.com>
2024-06-26 20:36:44 +0300

    fix: task deadlineSchema

--- api-server/src/models/task.js
+++ api-server/src/models/task.js
@@ -18,6 +18,8 @@ taskPrioritySchema.post("save", handleMongooseError);
 
 const TaskPriority = model("task_priority", taskPrioritySchema);
 
+const deadlineReg = /^(\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
+
 const taskSchema = new Schema(
   {
     name: {
@@ -31,7 +33,8 @@ const taskSchema = new Schema(
       default: null,
     },
     deadline: {
-      type: Date,
+      type: String,
+      match: deadlineReg,
       required: true,
     },
     priority_id: {
@@ -60,7 +63,8 @@ const Task = model("task", taskSchema);
 
 const nameSchema = Joi.string().trim().max(255).required();
 const commentSchema = Joi.string().trim().max(1000);
-const deadlineSchema = Joi.date()
+const deadlineSchema = Joi.string()
+  .pattern(deadlineReg)
   .custom((value, helpers) => {
     const currentUTCDatetime = new Date();
     const currentUTCDate = new Date(
@@ -71,11 +75,21 @@ const deadlineSchema = Joi.date()
       )
     );
 
-    if (value >= currentUTCDate) {
-      return value;
-    }
+    const parsedValue = new Date(value);
     const fieldName = helpers.state.path.join(".");
-    return helpers.message(`\"${fieldName}\" should be a date in the future`);
+    const [valueYear, valueMonth, valueDate] = value.split("-");
+
+    if (
+      parsedValue.getUTCFullYear() !== Number(valueYear) ||
+      parsedValue.getUTCMonth() !== Number(valueMonth) - 1 ||
+      parsedValue.getUTCDate() !== Number(valueDate)
+    ) {
+      return helpers.message(`\"${fieldName}\" invalide date`);
+    }
+    if (parsedValue < currentUTCDate) {
+      return helpers.message(`\"${fieldName}\" should be a date in the future`);
+    }
+    return value;
   })
   .required();
 const priorityIdSchema = Joi.string().custom((value, helpers) => {
