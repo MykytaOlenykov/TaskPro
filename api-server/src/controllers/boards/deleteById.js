f8e35286de4c470d0d7afe74f4d8d6937867790d
Mykyta <nikita2001395@gmail.com>
2024-06-27 16:40:22 +0300

    change: board deleteById controller

--- api-server/src/controllers/boards/deleteById.js
+++ api-server/src/controllers/boards/deleteById.js
@@ -1,14 +1,27 @@
 const { Board } = require("../../models/board");
+const { Column } = require("../../models/column");
+const { Task } = require("../../models/task");
 const { HttpError } = require("../../helpers");
 
 const deleteById = async (req, res) => {
   const { id } = req.params;
   const { _id: owner_id } = req.user;
-  const result = await Board.findOneAndDelete({ _id: id, owner_id });
+  const board = await Board.exists({ _id: id, owner_id });
 
-  if (!result) {
+  if (!board) {
     throw HttpError(404);
   }
+
+  const columns = await Column.find({ board_id: board._id, owner_id });
+
+  if (columns.length > 0) {
+    const columnIds = columns.map((column) => column._id);
+
+    await Task.deleteMany({ column_id: { $in: columnIds }, owner_id });
+    await Column.deleteMany({ board_id: board._id, owner_id });
+  }
+
+  await Board.deleteOne({ _id: id, owner_id });
 
   res.status(204).send();
 };
