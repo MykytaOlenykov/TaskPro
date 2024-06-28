598fb441a475dd661490f7f65d0b8d9948be4c2a
Mykyta <nikita2001395@gmail.com>
2024-06-28 22:58:36 +0300

    remove: validateQuery.js

--- api-server/src/middlewares/index.js
+++ api-server/src/middlewares/index.js
@@ -1,13 +1,11 @@
 const validateBody = require("./validateBody");
 const authenticate = require("./authenticate");
 const isValidObjectId = require("./isValidObjectId");
-const validateQuery = require("./validateQuery");
 const verifyRefreshToken = require("./verifyRefreshToken");
 
 module.exports = {
   validateBody,
   authenticate,
   isValidObjectId,
-  validateQuery,
   verifyRefreshToken,
 };
