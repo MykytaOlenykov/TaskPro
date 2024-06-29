c71aaa8d686b326eb2e1ae7be477e9ef0abc8cf3
Mykyta <nikita2001395@gmail.com>
2024-06-29 12:38:23 +0300

    add: base routes in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/welcome" element={<p>/welcome</p>} />
+      <Route path="/auth/:id" element={<p>/auth/:id</p>} />
+
+      <Route path="/home" element={<p>/home</p>}>
+        <Route index element={<p>index</p>} />
+        <Route path=":boardId" element={<p>boardId</p>} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
