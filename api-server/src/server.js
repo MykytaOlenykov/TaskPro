bb6b97d715316cccb247fcd6b0fec4bce890a468
Mykyta <nikita2001395@gmail.com>
2024-07-24 13:21:58 +0300

    add: docker build

--- api-server/src/server.js
+++ api-server/src/server.js
@@ -2,14 +2,16 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { MONGO_DB_HOST, PORT = 8000 } = process.env;
+const { MONGO_DB_HOST, API_SERVER_PORT = 8000 } = process.env;
 
 mongoose
-  .connect(MONGO_DB_HOST)
+  .connect(MONGO_DB_HOST, {
+    authSource: "admin",
+  })
   .then(() => {
     console.log("Database connection successful");
-    app.listen(PORT, () => {
-      console.log(`Server running. Use our API on port: ${PORT}`);
+    app.listen(API_SERVER_PORT, () => {
+      console.log(`Server running. Use our API on port: ${API_SERVER_PORT}`);
     });
   })
   .catch((error) => {
